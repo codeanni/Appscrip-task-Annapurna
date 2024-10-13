81bd5819013cb230b3b3b3c0d74ae9bcfa886743
Annapurna Priyadarsini Mallick <mallickannapurna3@gmail.com>
2024-10-13 12:57:11 +0530

    Updated the project with new changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Logo from '../Assets/Logo.png';
 
 const Header = () => {
   return (
     <header className='bg-light p-3'>
         <div className='d-flex justify-content-between align-items-center px-5'>
-            <div className='logo'>Your logo</div>
+            <div className='logo'>
+                <img src={Logo} 
+                style={{
+                    height: '30px',
+                    width: '30px',
+                    backgroundColor: 'red',}}
+                    />
+            </div>
             <div className='text-center fw-bold fs-1'>Logo</div>
             <div className='d-flex gap-3'>
                 <i className='fas fa-search'></i>
